28cb234be3aaf7fea6cb218facad7125ea82c597
terrance <terrance.wang@dream-ai.com>
2024-05-31 12:44:47 -0700

    try again

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup
+# from setuptools import setup
 
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
+# with open("requirements.txt") as f:
+#     required = f.read().splitlines()
 
-if __name__ == "__main__":
-    setup(
-        name="practical_rife",
-        package_data={
-            "practical_rife": [
-                "**/*.txt",
-                "**/*.t7",
-                "**/*.pth",
-                "**/*.json",
-                "**/*.yaml",
-                "**/*.yml",
-            ]
-        },
-    )
+# if __name__ == "__main__":
+#     setup(
+#         name="practical_rife",
+#         package_data={
+#             "practical_rife": [
+#                 "**/*.txt",
+#                 "**/*.t7",
+#                 "**/*.pth",
+#                 "**/*.json",
+#                 "**/*.yaml",
+#                 "**/*.yml",
+#             ]
+#         },
+#     )
